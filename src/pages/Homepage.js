3688aa2bca0a255f4729138c8bcf76aa3b3c0a4a
mifranco-upenn <112339889+mifranco-upenn@users.noreply.github.com>
2023-11-27 13:55:07 -0500

    improved layout content

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Bag, HouseFill, ListTask, ClipboardCheck, CalendarWeek, Map } from "react-bootstrap-icons";
+import { Link } from "react-router-dom";
 import "./Homepage.css"
 
 
@@ -6,29 +8,59 @@ const Homepage = () => {
     return (
         <div className="screen-container">
             <div className="top-banner">
-                {/* Content for the welcome banner */}
-                <h2>Welcome to My App!</h2>
+                <div className="banner-content">
+                    <img src="/movaid-icon.png" alt="Movaid Icon" className="app-icon" />
+                    <h2 className="welcome-header">Good Evening, John!</h2>
+                    <p className="subheader">You are <strong>3 days</strong> away from moving into 123 Main Street</p>
+                </div>
             </div>
 
             <div className="scrollable-content">
+                <h5 className="widget-title">Things to Purchase</h5>
                 <div className="widget-container">
                     <p>Widget 1</p>
                 </div>
+                <h5 className="widget-title">To-Do</h5>
                 <div className="widget-container">
                     <p>Widget 2</p>
                 </div>
+                <h5 className="widget-title">Move-In Plan</h5>
                 <div className="widget-container">
                     <p>Widget 3</p>
                 </div>
+                <h5 className="widget-title">Discover Philadelphia</h5>
                 <div className="widget-container">
                     <p>Widget 4</p>
                 </div>
+                
             </div>
 
             <div className="bottom-bar">
-                {/* Content for the bottom navigation bar */}
-                {/* You can add icons and links for different pages */}
-                <span>Home</span> | <span>Page 1</span> | <span>Page 2</span>
+                <Link to="/" style={{color: 'inherit'}}>
+                    <div className="icon-wrapper">
+                        <HouseFill size={40} />
+                    </div>
+                </Link>
+                <Link to="/prototype-purchase-screen" className="navbar-link">
+                    <div className="icon-wrapper">
+                        <Bag size={30} />
+                    </div>
+                </Link>
+                <Link to="/prototype-todo-screen" className="navbar-link">
+                    <div className="icon-wrapper">
+                        <ClipboardCheck size={30} />
+                    </div>
+                </Link>
+                <Link to="/prototype-movein-plan-screen" className="navbar-link">
+                    <div className="icon-wrapper">
+                        <CalendarWeek size={30} />
+                    </div>
+                </Link>
+                <Link to="/prototype-getting-around-screen" className="navbar-link">
+                    <div className="icon-wrapper">
+                        <Map size={30} />
+                    </div>
+                </Link>
             </div>
         </div>
     );
