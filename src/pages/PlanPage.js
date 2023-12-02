918dbbb8d3227504e6e5cb1ed66e4598d4be37a4
mifranco-upenn <112339889+mifranco-upenn@users.noreply.github.com>
2023-12-02 11:22:31 -0500

    plan GET & POST

--- src/pages/PlanPage.js
+++ src/pages/PlanPage.js
@@ -3,18 +3,79 @@ import "./PlanPage.css";
 import { Bag, House, Map, ClipboardCheck, CalendarWeekFill } from "react-bootstrap-icons";
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { Button, ListGroup } from "react-bootstrap";
+import { Button, ListGroup, Form, Modal } from "react-bootstrap";
+import { v4 as uuidv4 } from 'uuid';
 
 
 const PlanPage = () => {
 
+    const [eventsList, setEventsList] = useState([]);
+
     const [addingEventModalOpen, setAddingEventModalOpen] = useState(false);
+    const [eventToAdd, setEventToAdd] = useState('');
+    const [eventToAddDateStart, setEventToAddDateStart] = useState(''); 
+    const [eventToAddDateEnd, setEventToAddDateEnd] = useState(''); 
+
+    useEffect(() => {
+        const fetchEvents = async () => {
+            try {
+                const response = await fetch('http://localhost:8000/moveInPlan');
+                const data = await response.json();
+                console.log(data);
+                setEventsList(data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchEvents();
+    }, []);
 
     // Sample data for demonstration
     const sampleEventData = [
         { date: "2023-12-01", weatherIcon: "🌤", highs: 25, lows: 15, events: ["Event 1", "Event 2"] },
         { date: "2023-12-02", weatherIcon: "🌤", highs: 30, lows: 10, events: ["Event 1", "Event 2"] },
     ];
+
+    const handleAddEvent = async () => {
+        try {
+            const startDate = new Date(eventToAddDateStart);
+            let endDate;
+
+            if (eventToAddDateEnd) {
+                endDate = new Date(eventToAddDateEnd);
+
+                if (endDate && startDate >= endDate) {
+                    alert('End date must be greater than the start date');
+                    return;
+                }
+            }
+            
+
+            const newEvent = { id: uuidv4(), desc: eventToAdd, dateStart: startDate.toISOString().split("T")[0], dateEnd: endDate ? endDate.toISOString().split("T")[0] : null, timeStart: startDate.toTimeString().split(" ")[0], timeEnd: endDate ? endDate.toTimeString().split(" ")[0] : null }
+
+            const response = await fetch(`http://localhost:8000/moveInPlan`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(newEvent),
+            });
+
+            if (response.ok) {
+                console.log('New event added successfully');
+
+                setEventToAdd('');
+                setEventToAddDateStart('');
+                setEventToAddDateEnd('');
+                setAddingEventModalOpen(false);
+            } else {
+                console.error('Failed to add event');
+            }
+        } catch (error) {
+            console.error('Error making POST request:', error);
+        }
+    }
 
     return (
         <div className="screen-container">
@@ -25,11 +86,40 @@ const PlanPage = () => {
                     <Button onClick={() => setAddingEventModalOpen(true)} variant="primary" style={{marginBottom: '10px'}}>
                         Add Event
                     </Button>
-                    <Button onClick={() => setAddingEventModalOpen(true)} variant="secondary" style={{marginBottom: '10px'}}>
+                    <Button variant="secondary" style={{marginBottom: '10px'}}>
                         View in Calendar
                     </Button>
                 </div>
             </div>
+
+            <Modal show={addingEventModalOpen} onHide={() => setAddingEventModalOpen(false)} >
+                <Modal.Header closeButton>
+                    <Modal.Title>Add a New Event</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group controlId="eventToAdd">
+                            <Form.Label>Event Description</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter event description"
+                                value={eventToAdd}
+                                onChange={(e) => setEventToAdd(e.target.value)}
+                            />
+                        </Form.Group>
+                        <Form.Group controlId="eventDateSelect">
+                            <Form.Label>Select Start Date & Time</Form.Label>
+                            <Form.Control type="datetime-local" onChange={(e) => setEventToAddDateStart(e.target.value)} />
+                            <Form.Label>Select End Date & Time</Form.Label>
+                            <Form.Control type="datetime-local" onChange={(e) => setEventToAddDateEnd(e.target.value)} />
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={() => setAddingEventModalOpen(false)}>Close</Button>
+                    <Button variant="primary" onClick={() => handleAddEvent()}>Add Item</Button>
+                </Modal.Footer>
+            </Modal>
 
             <div className="plan-scrollable-content">
                 {sampleEventData.map((data, index) => (
